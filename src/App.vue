57d27ac244dd700792f457f5f810911b8cc2b0ef
Shahadat Hosain <shahadat80@gmail.com>
2023-08-08 23:56:32 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,96 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, reactive } from 'vue'
+
+const users = ref([])
+
+const registerForm = reactive({
+  email: '',
+  password: ''
+})
+
+const loginForm = reactive({
+  email: '',
+  password: ''
+})
+
+const register = () => {
+  if(registerForm.email == '') {
+    alert('Email is required');
+  }else if(registerForm.password == '') {
+    alert('Password is required');
+  }else{
+    users.value.push(registerForm)
+    formStatus.logedIn = false
+    formStatus.register = false
+    formStatus.login = true
+  }
+  
+}
+
+const login = () => {
+  if(loginForm.email == '') {
+    alert('Email is required');
+  }else if(loginForm.password == '') {
+    alert('Password is required');
+  }else{
+    const foundUser = users.value.find(item => item.email == loginForm.email && item.password == loginForm.password);
+    if(foundUser){
+      formStatus.logedIn = true
+      formStatus.register = false
+      formStatus.login = false
+    }
+    else{
+      alert('User or password is incorrect');
+    }
+  }
+}
+
+const formStatus = reactive({
+  login: false,
+  register: true,
+  logedIn: false
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div class="container">
+  <div v-if="formStatus.register == true" class="bg-white shadow-md rounded-md p-6 w-6/12">
+      <h2 class="text-2xl font-bold mb-4">Registration</h2>
+      <form @submit.prevent="register">
+        <div class="mb-4">
+          <label for="email" class="block font-medium">Email:</label>
+          <input type="email" id="email" v-model="registerForm.email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600  border-solid rounded-md border-gray-300 focus:border-indigo-500 focus:ring">
+        </div>
+        <div class="mb-4">
+          <label for="password" class="block font-medium">Password:</label>
+          <input type="password" id="password" v-model="registerForm.password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600  border-solid rounded-md border-gray-300 focus:border-indigo-500 focus:ring">
+        </div>
+        <button type="submit" class="w-full bg-indigo-500 text-white font-bold py-2 px-4 rounded-md">Register</button>
+      </form>
+    </div>
+    <div class="mx-4"></div>
+    <div v-if="formStatus.login == true" class="bg-white shadow-md rounded-md p-6 w-6/12">
+      <h2 class="text-2xl font-bold mb-4">Login</h2>
+      <form @submit.prevent="login">
+        <div class="mb-4">
+          <label for="email" class="block font-medium">Email:</label>
+          <input type="email" id="email" v-model="loginForm.email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600  border-solid rounded-md border-gray-300 focus:border-indigo-500 focus:ring">
+        </div>
+        <div class="mb-4">
+          <label for="password" class="block font-medium">Password:</label>
+          <input type="password" id="password" v-model="loginForm.password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600  border-solid rounded-md border-gray-300 focus:border-indigo-500 focus:ring">
+        </div>
+        <button type="submit" class="w-full bg-indigo-500 text-white font-bold py-2 px-4 rounded-md">Login</button>
+      </form>
+    </div>
+
+    <div v-if="formStatus.logedIn == true" class="bg-white shadow-md rounded-md p-6">
+      <h2 class="text-2xl font-bold mb-4">You are logged in</h2>
+    </div>
+</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,18 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    <h2>List of Items</h2>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        <strong>Name:</strong> {{ item.name }} <strong>Age:</strong> {{ item.age }}
+      </li>
+    </ul>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<script setup>
+  const items = [
+        { name: 'John', age: 25 },
+        { name: 'Jane', age: 30 },
+        { name: 'Bob', age: 40 },
+      ]
+</script>
